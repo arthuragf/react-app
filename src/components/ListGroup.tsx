b892976f2ca94945c8d606134fd1dd36de75fff1
Arthur Faria <arthur.agf@gmail.com>
2025-06-19 22:30:13 -0300

    Add message when the list is empty

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -4,6 +4,7 @@ function ListGroup() {
   return (
     <>
       <h1>List</h1>
+      {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
         {items.map((item) => (
           <li className="list-group-item" key={item}>
