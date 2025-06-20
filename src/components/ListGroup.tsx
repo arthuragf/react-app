4e31c8ceb730e6cb3e11b9aa4edbf06ea53eaac7
Arthur Faria <arthur.agf@gmail.com>
2025-06-19 23:11:22 -0300

    Make list component reusable

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,12 +1,15 @@
 import { useState } from "react";
+interface ListGroupProps {
+  items: string[];
+  heading: string;
+}
 
-function ListGroup() {
-  const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+function ListGroup({ items, heading }: ListGroupProps) {
   const [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
     <>
-      <h1>List</h1>
+      <h1>{heading}</h1>
       {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
         {items.map((item, index) => (
